7e85dc6ea6f6181241eb03e69fed0f94f53ef63b
pavelihno <crazzyyellow02@gmail.com>
2023-11-10 23:53:46 +0300

    created structure for spring apps

---
+++ practice_8/app/catalog/src/main/groovy/com/mirea/book/catalog/CatalogApplication.groovy
@@ -0,0 +1,13 @@
+package com.mirea.book.catalog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CatalogApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CatalogApplication, args)
+	}
+
+}
---
+++ practice_8/app/order/src/main/groovy/com/mirea/book/order/OrderApplication.groovy
@@ -0,0 +1,13 @@
+package com.mirea.book.order
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OrderApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(OrderApplication, args)
+	}
+
+}
