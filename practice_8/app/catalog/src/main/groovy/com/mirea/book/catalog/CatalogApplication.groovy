8166a0cfed623b42ed0b1ab2d57890a70778dc17
pavelihno <crazzyyellow02@gmail.com>
2023-12-12 16:56:05 +0300

    jaeger created

--- practice_8/app/catalog/src/main/groovy/com/mirea/book/catalog/CatalogApplication.groovy
+++ practice_8/app/catalog/src/main/groovy/com/mirea/book/catalog/CatalogApplication.groovy
@@ -2,6 +2,10 @@ package com.mirea.book.catalog
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
 
 @SpringBootApplication
 class CatalogApplication {
@@ -10,4 +14,8 @@ class CatalogApplication {
 		SpringApplication.run(CatalogApplication, args)
 	}
 
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value('${tracing.url}') String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build()
+	}
 }
--- practice_8/app/notifications/src/main/groovy/com/mirea/book/Notifications/NotificationsApplication.groovy
+++ practice_8/app/notifications/src/main/groovy/com/mirea/book/Notifications/NotificationsApplication.groovy
@@ -2,6 +2,10 @@ package com.mirea.book.notifications
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
 
 @SpringBootApplication
 class NotificationsApplication {
@@ -10,4 +14,8 @@ class NotificationsApplication {
 		SpringApplication.run(NotificationsApplication, args)
 	}
 
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value('${tracing.url}') String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build()
+	}
 }
